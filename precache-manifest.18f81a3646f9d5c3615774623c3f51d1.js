bc2cee35e936b5899af5d1fa3ed74b34dcf9cf16
pranav_j <pranav.jawaji_c@shoptimize.ai>
2021-06-30 14:20:27 +0530

    Updates

---
+++ precache-manifest.18f81a3646f9d5c3615774623c3f51d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8edead2372c66cf0010659b82065c6d9",
+    "url": "/markdown-editor/index.html"
+  },
+  {
+    "revision": "66fba5e9872242320b67",
+    "url": "/markdown-editor/static/css/main.6848d9ca.chunk.css"
+  },
+  {
+    "revision": "306b1d3ce0c4055a86df",
+    "url": "/markdown-editor/static/js/2.45fe41f0.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/markdown-editor/static/js/2.45fe41f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66fba5e9872242320b67",
+    "url": "/markdown-editor/static/js/main.fc93a243.chunk.js"
+  },
+  {
+    "revision": "8820c5d914fda841c723",
+    "url": "/markdown-editor/static/js/runtime-main.10523d88.js"
+  }
+]);
